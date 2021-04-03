400863aba2edfc7f4e5fd9c354f5225b4d9ee797
MatchaCrisp <junyanbill@hotmail.com>
2021-04-03 19:36:04 -0400

    apr 3 2021

--- src/index.js
+++ src/index.js
@@ -5,9 +5,24 @@ import './index.css';
 
 
 const Backdrop=()=>{
+    const randomize=ranMax=> Math.floor(Math.random*ranMax);
+
+
+
+    return (
+        <div className="backdrop">
+
+            <QuoteBox/>
+        </div>
+    )
+};
+
+const QuoteBox=props=>{
     const [quotes,setQuotes]=useState({});
     const [status,setStatus]=useState('idle');
+    const [quotePack,setQuotePack]=useState({});
     const [url,setUrl]=useState("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json");
+    let quoteCont;
     useEffect(()=>{
         if (!url)return;
         const fetchData=async ()=>{
@@ -24,23 +39,17 @@ const Backdrop=()=>{
         fetchData().catch(console.log);
 
     },[url]);
+    useEffect(()=>{
+        if (status==='fetching')
+            quoteCont=<img src="img/loading.gif" className="loading" alt="loading symbol"></img>
+        else if (status==='fetched')
+            quoteCont=<div id="quoteWrap"><p id="text">ipsumy</p>
+            <p id="author">ippy</p></div>
 
-
-
+    },[status]);
     return (
-        <div>
-
-            <QuoteBox quotes={quotes}/>
-        </div>
-    )
-};
-
-const QuoteBox=props=>{
-    console.log(props.quotes.quotes);
-
-    return (
-        <div className='quoteBox' >
-
+        <div className='quoteBox' id='quote-box'>
+            {quoteCont}
             <TweetButton />
             <NewQuoteButton />
         </div>
