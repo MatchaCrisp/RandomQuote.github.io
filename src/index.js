fc28669311de107db490f4b9eebd84e1c9abd882
MatchaCrisp <junyanbill@hotmail.com>
2021-04-08 22:52:08 -0400

    loading,pause,style,animation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 const Backdrop=()=>{
     //what color theme to use 
-    const [theme,setTheme]=useState(1);
+    const [theme,setTheme]=useState(0);
     //the entirety of quotes.json
     const [quotes,setQuotes]=useState([]);
     //data fetching/quote setting status
@@ -16,11 +16,12 @@ const Backdrop=()=>{
     //url of quotes.json
     const [url]=useState("https://raw.githubusercontent.com/MatchaCrisp/RandomQuote.github.io/main/src/data/quotes.json");
 
+    let prevC=0;
     //fetching quotes.json
     useEffect(()=>{
         if (!url)return;
         const fetchData=async ()=>{
-            setStatus('busy');
+            setStatus('fetching');
                 const response=await fetch(url);
                 const data=await response.json();
 
@@ -38,6 +39,8 @@ const Backdrop=()=>{
 
         setTheme(randomize(9));
         setQuotePack(quotes[randomize(quotes.length)]);
+        
+        
     },[quotes]);
 
     //loading screen at beginning while fetching, disable buttons while setting quotes
@@ -49,64 +52,88 @@ const Backdrop=()=>{
     const randomize=ranMax=> Math.floor(Math.random()*ranMax);
 
     //onclick for when new quote button is pressed
-    const onNewQuote=()=>{
+    const onNewQuote= async()=>{
         setStatus('busy');
         setTheme(randomize(9));
         setQuotePack(quotes[randomize(quotes.length)]);
+        await new Promise(resolve => setTimeout(resolve, 400));
+        prevC=theme;
+
         setStatus('idle');
     };
 
+    
+    
     return (
         <div 
-            id="backdrop" 
-            style={{backgroundColor:`var(--col-pri-${theme})`}}>
-                <QuoteBox getQuote={onNewQuote} quote={quotePack} theme={theme}/>
+            id="backdrop"
+            style={{backgroundColor:status==="busy"?prevC:`var(--col-pri-${theme})`}} 
+            >
+                <Expander theme={theme} status ={status} />
+                <QuoteBox getQuote={onNewQuote} quote={quotePack} theme={theme} status={status}/>
         </div>
     )
 };
-
+const Expander =props =>{
+    return (
+        <div id="backAnim"
+            style={{backgroundColor:`var(--col-pri-${props.theme})`,
+                    animation:props.status==="busy"?'expand 450ms':'none'}} />
+    )
+}
 const QuoteBox=props=>{
+    const q = <div id="contQuote">
+                <p id="text">{props.quote.quote}</p>
+                <p id="author">-{props.quote.author}</p>
+            </div>
+    const b = <img src="https://media0.giphy.com/media/3o7TKtnuHOHHUjR38Y/giphy.gif" alt="loading" className="large centered"/>
     return (
         <div 
             id='quote-box' 
-            style={{backgroundColor:`var(--col-acc1-${props.theme})`,color:`var(--col-txt1-${props.theme})`}}>
-                <div id="contQuote">
-                    <p id="quote">{props.quote.quote}</p>
-                    <p id="author">-{props.quote.author}</p>
-                </div>
-                <div id="contButton">
-                    <TweetButton theme={props.theme} quote={props.quote}/>
-                    <NewQuoteButton onClick={props.getQuote} theme={props.theme}/>
-                </div>
+            style={{backgroundColor:`var(--col-acc1-${props.theme})`,
+                    color:`var(--col-txt1-${props.theme})`,
+                    animation:props.status==="busy"?window.innerWidth>=769?`bouncy2 400ms`:`bouncy 400ms`:'none'}}>
+
+                    {props.status!=='idle'?b:q}
+                    
+                    <div id="contButton">
+                        <TweetButton theme={props.theme} quote={props.quote} status={props.status}/>
+                        <NewQuoteButton onClick={props.getQuote} theme={props.theme} status={props.status}/>
+                    </div>
+
 
         </div>
     )
 };
 
 const TweetButton=props=>{
+    const b = <img src="https://media0.giphy.com/media/3o7TKtnuHOHHUjR38Y/giphy.gif" alt="loading" className="small"/>
+    const t= <i className="fab fa-twitter"></i>
     const tweetUrl=`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent('"'+props.quote.quote+'" '+props.quote.author)}`;
     return (
         <a 
-            id='tweetButton'
-            className='button' 
-            href={tweetUrl}
+            id='tweet-quote'
+            className={props.status!=='idle'?'disableButton':'button'} 
+            href={props.status!=='idle'?'#':tweetUrl}
             target="_blank"
-            style={{backgroundColor:`var(--col-pri-${props.theme})`,
+            rel="noreferrer"
+            style={{backgroundColor:props.status!=='idle'?"grey":`var(--col-pri-${props.theme})`,
                     color:`var(--col-acc2-${props.theme})`}}>
-                <i class="fab fa-twitter"></i>
+                {props.status!=='idle'?b:t}
         </a>
     )
 };
 
 const NewQuoteButton=props=>{
+    const b = <img src="https://media0.giphy.com/media/3o7TKtnuHOHHUjR38Y/giphy.gif" alt="loading" className="small"/>
     return (
         <button 
-            id='newQuoteButton'
-            className='button' 
-            onClick={props.onClick} 
-            style={{backgroundColor:`var(--col-pri-${props.theme})`,
+            id='new-quote'
+            className={props.status!=='idle'?'disableButton':'button'} 
+            onClick={props.status!=='idle'?null:props.onClick} 
+            style={{backgroundColor:props.status!=='idle'?"grey":`var(--col-pri-${props.theme})`,
                     color:`var(--col-acc2-${props.theme})`}}>
-                New Quote
+                {props.status!=='idle'?b:'New Quote'}
         </button>
     )
 };
